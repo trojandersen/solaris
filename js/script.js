47fddc25e978b0577b6507b3298ff0202ae3f9f4
Troj <trojandersen@gmail.com>
2024-02-21 14:10:54 +0100

    Added basic styling & html

--- js/script.js
+++ js/script.js
@@ -2,17 +2,18 @@ const baseURL = "https://n5n3eiyjb0.execute-api.eu-north-1.amazonaws.com";
 
 // Main function which fetches the API
 async function fetchAPI() {
-    try {
-        const resp = await fetch(`${baseURL}/bodies`, {
-            method: 'GET',
-            headers: {'x-zocom': apiKey}
-        })
-        const {bodies} = await resp.json();
-        console.log(bodies);
-    }  
-    catch (error) {
-        console.error(error);
+  try {
+    const resp = await fetch(`${baseURL}/bodies`, {
+      method: "GET",
+      headers: { "x-zocom": apiKey },
+    });
+    const { bodies } = await resp.json();
+    for (const i of bodies) {
+      console.log(i);
     }
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 fetchAPI();
